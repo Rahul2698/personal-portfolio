9e102d52156c4807e20d5052a25f291bf74de14a
Rahul Chatterjee <chatterjeerahul187@gmail.com>
2022-07-17 15:58:35 +0530

    link added to see detailed blog post

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 
 def all_blogs(request):
     blog_posts = BlogPost.objects.all()
-    return render(request,  'blogpost/all_blogs.html', {'blog_posts':blog_posts})
+    return render(request,  'blogpost/all_blogs.html', {'blog_posts': blog_posts})
+
+
+def details(request, blog_id):
+    blog = get_object_or_404(BlogPost,pk=blog_id)
+    return render(request, 'blogpost/details.html', {'blog': blog})
+
